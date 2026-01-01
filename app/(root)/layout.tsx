298e3340c087d83ed5c4890a8473eb0559713176
Kuldeep Saini <k72125642@gmail.com>
2026-01-02 00:16:08 +0530

    add navbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
+import Footer from '@/components/footer'
+import Navbar from '@/components/navbar'
+
 const layout = ({ children}: {children: React.ReactNode}) => {
   return (
     <>
-    Navbar
+    <Navbar/>
     {children}
-    Footer
+    <Footer />
     </>
   )
 }
